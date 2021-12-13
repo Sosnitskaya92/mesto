a68c8825a214dbe85ca606627a8f4ef9e7158b61
Angelina <Sosnitskaya92@mail.ru>
2021-12-13 11:08:06 +0300

    feat: Модальное добавления места

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,17 @@
+ 
 const editBtn = document.querySelector('.profile__edit');
-const popup = document.querySelector('.popup');
-const closeBtn = document.querySelector('.popup__close');
-const formElement = document.querySelector('.popup__form')
+const popup = document.querySelector('.popup__edit');
+const closeBtn = document.querySelector('.popup__close_edit');
+const formElement = document.querySelector('.popup__form_edit');
 const namePopup = document.querySelector('.popup__input_type_name');
 const jobPopup = document.querySelector('.popup__input_type_info');
 const nameProfile = document.querySelector('.profile__title');
 const jobProfile = document.querySelector('.profile__text');
+const popupAdd = document.querySelector('.popup__add');
+const formAdd = document.querySelector('.popup__form_add');
+const addBtn = document.querySelector('.profile__add');
+const closeAddBtn = document.querySelector('.popup__close_add');
+
 
 function openPopup() {
   popup.classList.add("popup_opened");
@@ -25,6 +31,24 @@ function formSubmitHandler (evt) {
   closePopup();
 }
 
+function OpenPopupAdd() {
+  popupAdd.classList.add("popup_opened"); 
+}
+
+function closePopupAdd() {
+  popupAdd.classList.remove("popup_opened");
+}
+
+function formSubmitHandlerAdd (evt) {
+  evt.preventDefault(); 
+  nameProfile.textContent = namePopup.value;
+  jobProfile.textContent = jobPopup.value;
+    //вызвали функцию закрытия попап окна
+  closePopup();
+}
+
+addBtn.addEventListener('click', OpenPopupAdd);
 editBtn.addEventListener('click', openPopup);
-closeBtn.addEventListener('click',closePopup);
+closeBtn.addEventListener('click', closePopup);
+closeAddBtn.addEventListener('click', closePopupAdd);
 formElement.addEventListener('submit', formSubmitHandler);
